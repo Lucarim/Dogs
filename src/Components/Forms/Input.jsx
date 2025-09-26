d64e416a4b58267caa8f0f1d132ed6e4618cbef5
Lucas Amorim <lucas--amorim@outlook.com>
2025-09-26 20:05:59 -0300

    useForm hook

--- src/Components/Forms/Input.jsx
+++ src/Components/Forms/Input.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import styles from "./Input.module.css";
 
-const Input = ({ label, type, name }) => {
+const Input = ({ label, type, name, value, onChange, error, onBlur }) => {
   return (
     <div className={styles.wrapper}>
       <label className={styles.label} htmlFor={name}>
         {label}
       </label>
-      <input className={styles.input} id={name} name={name} type={type} />
-      <p className={styles.error}>Error</p>
+      <input
+        className={styles.input}
+        id={name}
+        value={value}
+        name={name}
+        type={type}
+        onChange={onChange}
+        onBlur={onBlur}
+      />
+      <p className={styles.error}>{error}</p>
     </div>
   );
 };
