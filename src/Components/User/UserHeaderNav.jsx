bb874b7891adbf2a4bbda300b7b070eafdcfe08b
Lucas Amorim <lucas--amorim@outlook.com>
2025-10-22 23:33:57 -0300

    user header pt1

---
+++ src/Components/User/UserHeaderNav.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { UserContext } from "../../UserContext";
+import MinhasFotos from "../../Assets/feed.svg?react";
+import Estatisticas from "../../Assets/estatisticas.svg?react";
+import AdicionarFoto from "../../Assets/adicionar.svg?react";
+import Sair from "../../Assets/sair.svg?react";
+
+const UserHeaderNav = () => {
+  const { userLogout } = React.useContext(UserContext);
+  return (
+    <nav>
+      <NavLink to="/conta">
+        <MinhasFotos />
+        Minhas Fotos
+      </NavLink>
+      <NavLink to="/conta/estatisticas">
+        <Estatisticas />
+        Estatísticas
+      </NavLink>
+      <NavLink to="/conta/postar">
+        <AdicionarFoto />
+        Adicionar Foto
+      </NavLink>
+      <button onClick={userLogout}>
+        <Sair />
+      </button>
+    </nav>
+  );
+};
+
+export default UserHeaderNav;
