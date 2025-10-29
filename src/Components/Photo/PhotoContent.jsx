10364a4dbff2cb7ee130ba79a27b010408c468d0
Lucas Amorim <lucas--amorim@outlook.com>
2025-10-28 22:57:35 -0300

    feed modal pt2

--- src/Components/Photo/PhotoContent.jsx
+++ src/Components/Photo/PhotoContent.jsx
@@ -12,7 +12,7 @@ const PhotoContent = ({ data }) => {
       </div>
       <div className={styles.details}>
         <div>
-          <p>
+          <p className={styles.author}>
             <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
             <span className={styles.views}>{photo.acessos}</span>
           </p>
@@ -22,7 +22,6 @@ const PhotoContent = ({ data }) => {
           <ul className={styles.attributes}>
             <li>{photo.peso} kg</li>
             <li>{photo.idade} anos</li>
-            <li>{photo.nome}</li>
           </ul>
         </div>
       </div>
